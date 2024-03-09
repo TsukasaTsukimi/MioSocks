678f6ba588581030f810bfef4567381e52876e4b
Tsukasa <Tsukasa.Tsukimi@outlook.com>
2024-03-09 16:39:21 +0800

    transfer to gostack

--- main.go
+++ main.go
@@ -1,26 +1,39 @@
 package main
 
 import (
-	"github.com/TsukasaTsukimi/MioSocks/core"
-	"github.com/TsukasaTsukimi/MioSocks/core/device/windivert"
-	"github.com/TsukasaTsukimi/MioSocks/core/option"
-	"github.com/xjasonlyu/tun2soe:\Github\shadow-main\shadow-main\pkgcks/v2/engine/mirror"
-	"gvisor.dev/gvisor/pkg/tcpip/stack"
+	"log"
+	"net"
+	"os"
+	"os/signal"
+	"syscall"
+
+	"github.com/TsukasaTsukimi/MioSocks/netstack"
+	"github.com/TsukasaTsukimi/MioSocks/tun"
 )
 
-var (
-	_defaultStack *stack.Stack
-	opts          []option.Option
-)
+type myHandler struct{}
+
+func (*myHandler) HandleTCPConn(info *netstack.ConnTuple, conn net.Conn) {
+	log.Printf("tcp, src: %s, dst: %s", info.Src(), info.Dst())
+	// do something...
+}
+func (*myHandler) HandleUDPConn(info *netstack.ConnTuple, conn net.PacketConn) {
+	log.Printf("udp, src: %s, dst: %s", info.Src(), info.Dst())
+	// do something...
+}
 
 func main() {
-	d := &windivert.Divert{}
-	_defaultStock, err := core.CreateStack(&core.Config{
-		LinkEndpoint:     d,
-		TransportHandler: &mirror.Tunnel{},
-		Options:          opts,
+	nt := netstack.New(tun.TunConfig{
+		Name: "tun2",
+		Addr: "192.18.0.1/16",
+		MTU:  tun.DefaultMTU,
 	})
-	if err != nil {
-		return
+	if err := nt.Start(); err != nil {
+		log.Fatal(err)
 	}
+	defer nt.Close()
+	nt.RegisterConnHandler(&myHandler{})
+	interrupt := make(chan os.Signal, 1)
+	signal.Notify(interrupt, syscall.SIGINT)
+	<-interrupt
 }
--- pkg/embed/embed.go
+++
@@ -1,6 +0,0 @@
-package embed
-
-import "embed"
-
-//go:embed admin.conns.html
-var Files embed.FS
