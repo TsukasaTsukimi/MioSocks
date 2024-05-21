0b6a4aa762882adcc73f75a26d5759c504411955
Tsukasa <Tsukasa.Tsukimi@outlook.com>
2024-05-21 21:06:17 +0800

    1. update Subscription tab 2. add interface library

---
+++ CommonLibrary/ServerInterface.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace CommonLibrary
+{
+    internal interface ServerInterface
+    {
+        string GetType();
+        string Edit(string json);
+        List<Dictionary<string, Func<string>>> MethodList();
+    }
+}
